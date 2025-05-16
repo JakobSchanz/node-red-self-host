3cee33d956580dd6167fcc1e6a97e95c0bbdf398
Jakob Schanz <Jakob Schanz>
2025-05-16 09:58:41 +0100

    login change

--- data/programm_data/node_red_settings/settings.js
+++ data/programm_data/node_red_settings/settings.js
@@ -6,20 +6,21 @@ module.exports = {
     userDir: path.resolve(__dirname, "../../.nodered"),    
     nodesDir: [path.resolve(__dirname, "../../custom-nodes")],  
 
-    adminAuth: {
-        type: "credentials",
-        users: [
-            {
-                username: "admin",
-                password: "$2b$08$HT8ohEib9yB6fzKdj95LHeKZZ.6Mx./xXNb56cLGFNaLkO7BR2Cmq", //jjj123
-                permissions: "*" //all permissions
-            },
-            {
-                username: "test",
-                password: "$2b$08$OgSuvr4P8h9CmDta7hk3nOuOWs8wkUjsML/l5ND7uDQvF1kzvhJ8W", //test
-                permissions: "read"
-            }
-            // to add new user password has to hash use: npx node-red admin hash-pw  
-        ]
-    },  
+     functionGlobalContext: {},
+
+  logging: {
+    console: {
+      level: "info",
+      metrics: false,
+      audit: false,
+    },
+  },
+
+  httpNodeCors: {
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+  },
+
+  editorTheme: {
+  },
 };
